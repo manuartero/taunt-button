4888f5979ad9661e157bbce9b70d45303bc8de12
Manu Artero Anguita <manutero.developer@gmail.com>
2022-10-13 15:18:19 +0200

    initial structure

---
+++ app.js
@@ -0,0 +1,4 @@
+import TauntButton from "./components/taunt-button";
+
+const btn = TauntButton();
+document.getElementById("root").appendChild(btn);
---
+++ components/taunt-button.js
@@ -0,0 +1,9 @@
+function TauntButton() {
+  const button = document.createElement("button");
+  button.className = "taunt-btn";
+
+  button.textContent = "Click Me";
+  return button;
+}
+
+export default TauntButton;
