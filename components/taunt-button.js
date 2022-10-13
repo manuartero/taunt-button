22604a837dd78c5ee4ce8868988b815241ce4eef
Manu Artero Anguita <manutero.developer@gmail.com>
2022-10-13 17:10:40 +0200

    pointer blocker between button and cursor

--- components/taunt-button.js
+++ components/taunt-button.js
@@ -1,4 +1,28 @@
+function PointerBlocker() {
+  const div = document.createElement("div");
+  div.className = "pointer-blocker";
+  div.classList.add("hidden");
+
+  return {
+    el: div,
+    setPosition(x, y) {
+      // div.style.left = x + "px";
+      // div.style.top = y + "px";
+      div.style.left = x - 12 + "px";
+      div.style.top = y - 12 + "px";
+    },
+    setVisible() {
+      div.classList.remove("hidden");
+    },
+    setInvisible() {
+      div.classList.add("hidden");
+    },
+  };
+}
+
 function TauntButton() {
+  const pointerBlocker = PointerBlocker();
+
   const button = document.createElement("button");
   button.className = "btn";
   button.disabled = true; // just in case user is able to click it somehow.
@@ -7,6 +31,35 @@ function TauntButton() {
   btnContent.className = "btn-content";
   btnContent.textContent = "Click Me";
   button.appendChild(btnContent);
+  button.appendChild(pointerBlocker.el);
+
+  const onEnter = (ev) => {
+    console.log(
+      `.btn » onEnter(${ev.relatedTarget?.className} => ${ev.target.className})`
+    );
+
+    pointerBlocker.setVisible();
+  };
+
+  const onHover = (ev) => {
+    const { clientX: x, clientY: y } = ev;
+    // console.log(`.btn » onHover(%s, %s)`, x, y);
+
+    pointerBlocker.setPosition(x, y);
+  };
+
+  const onLeave = (ev) => {
+    console.log(
+      `.btn » onLeave(${ev.target.className} => ${ev.relatedTarget?.className})`
+    );
+
+    pointerBlocker.setInvisible();
+  };
+
+  button.addEventListener("mousemove", onHover);
+
+  button.addEventListener("mouseover", onEnter);
+  button.addEventListener("mouseout", onLeave);
 
   return { el: button };
 }
